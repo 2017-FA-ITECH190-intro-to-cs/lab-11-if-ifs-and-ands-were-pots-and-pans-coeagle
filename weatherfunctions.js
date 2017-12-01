87a6ef8c5f94ab8baf6606f195f0f15dcb61daa0
colin eagle <coeagle@students.lltc.edu>
2017-12-01 02:59:07 +0000

    i do not know why this isnt working

--- weatherfunctions.js
+++ weatherfunctions.js
@@ -1,11 +1,12 @@
 function calculateDewPoint (temp, humidity)
-{var dewp = parseFloat(document.getElementById('humidityBox').value);
-            if (isNaN(dewp) || dewp < 50) 
-                {alert("invalid");
-                return
-            ;}
-                
-            
+{
+       
+      if (humidity <= 50)
+      {
+         alert("You made a mistake!.");
+         return("invalid");
+      }
+        
     var dewp;
     dewp = temp - 9 * (100 - humidity)/25;
     return dewp; 
@@ -13,6 +14,11 @@ function calculateDewPoint (temp, humidity)
 
 function calculateWindChill (temp, wind)
 {
+    if (temp <= 50)
+    {
+        alert("Your a little short YO!!.");
+        return("invalid");
+    }
     var winchill;
     winchill = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow (wind,0.16) ;
     return winchill;
