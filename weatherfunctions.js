118a43c5215d166e32e319d246b40470d9aa24b1
colin eagle <coeagle@students.lltc.edu>
2017-12-01 03:04:02 +0000

    a little bit more of a finished product

--- weatherfunctions.js
+++ weatherfunctions.js
@@ -19,6 +19,10 @@ function calculateWindChill (temp, wind)
         alert("Your a little short YO!!.");
         return("invalid");
     }
+    {
+        if (wind <= 3)
+        return("uh oh dummy")
+    }
     var winchill;
     winchill = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow (wind,0.16) ;
     return winchill;
